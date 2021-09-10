e185a8a27da1ac9060e07c5d89199e2f565e1989
bhaweshJ <joshibj@mail.uc.edu>
2021-09-10 19:43:40 -0400

    Task 3 Assignment2 for week 2

---
+++ Week1/Week2/RecruitCatjoshibj/Pages/Team_members/bhawesh.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RecruitCatjoshibj.Pages.Team_members
+{
+    public class bhaweshModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
